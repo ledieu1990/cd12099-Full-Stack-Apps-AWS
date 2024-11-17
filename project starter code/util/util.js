9040cc249901378674eedf4af17d43ccc165bc24
Le Dieu <hongledt1905@gmail.com>
2024-11-17 19:14:59 +0700

    add/update code for submission

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import Jimp from "jimp";
-
+import axios from "axios";
+import { response } from "express";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,10 +10,15 @@ import Jimp from "jimp";
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
- export async function filterImageFromURL(inputURL) {
+export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const response = await axios({
+        method: "get",
+        url: inputURL,
+        responseType: "arraybuffer",
+      });
+      const photo = await Jimp.read(response.data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
@@ -22,6 +28,7 @@ import Jimp from "jimp";
         .write(outpath, (img) => {
           resolve(outpath);
         });
+      console.log(outpath);
     } catch (error) {
       reject(error);
     }
@@ -33,7 +40,7 @@ import Jimp from "jimp";
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
- export async function deleteLocalFiles(files) {
+export async function deleteLocalFiles(files) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
